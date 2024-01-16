b1d4fb2da7c3c53b99a623f4fe9514f7b7bf5ce4
조현진 <chohyeonjin@johyeonjin-ui-MacBookPro.local>
2024-01-16 18:55:47 +0900

    props drilling의 시작

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,10 @@
+export default function Avatar({ image, isNew }) {
+  return (
+    <>
+      <div className="avatar">
+        <img src={image} alt="avatar" className="photo" />
+        {isNew && <span className="new">New</span>}
+      </div>
+    </>
+  );
+}
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,12 @@
-export default function Profile(props) {
+import Avatar from "./Avatar";
+
+export default function Profile({ image, name, title, isNew }) {
   return (
     <>
       <div className="profile">
-        <img src={props.image} alt="avatar" className="photo" />
-        {props.isNew && <span className="new">New</span>}
-        <h1>{props.name}</h1>
-        <p>{props.title}</p>
+        <Avatar image={image} isNew={isNew} />
+        <h1>{name}</h1>
+        <p>{title}</p>
       </div>
     </>
   );
